9ea5ccf473cce5e1cadfdc707102425db89d403a
Maggie Xu <maggiex@canva.com>
2025-07-13 16:13:41 +1000

    remove prettier and format

--- src/game/characters/Colonel.ts
+++ src/game/characters/Colonel.ts
@@ -1,26 +1,26 @@
 import { Scene } from "phaser";
 
 class Colonel extends Phaser.GameObjects.Sprite {
-    constructor(scene: Scene, x: number, y: number) {
-        super(scene, x, y, 'colonel');
+  constructor(scene: Scene, x: number, y: number) {
+    super(scene, x, y, 'colonel');
 
-    }
+  }
 
-    static preload(scene: Scene) {
-        scene.load.spritesheet('colonel', 'assets/colonel.png', {
-            frameWidth: 230,
-            frameHeight: 215
-        });
-    }
-    
-    createWalk(scene: Scene) {
-        scene.anims.create({
-            key: 'walk',
-            frames: scene.anims.generateFrameNumbers('colonel', { start: 4, end: 7 }),
-            frameRate: 8,
-            repeat: -1
-        });
-    }
+  static preload(scene: Scene) {
+    scene.load.spritesheet('colonel', 'assets/colonel.png', {
+      frameWidth: 230,
+      frameHeight: 215
+    });
+  }
+
+  createWalk(scene: Scene) {
+    scene.anims.create({
+      key: 'walk',
+      frames: scene.anims.generateFrameNumbers('colonel', { start: 4, end: 7 }),
+      frameRate: 8,
+      repeat: -1
+    });
+  }
 }
 
 export default Colonel;
