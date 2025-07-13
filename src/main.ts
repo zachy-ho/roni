7b0e1fd90b396694d4215f7197d19422502b8006
Zachary Ho <zyyho97@gmail.com>
2025-07-13 17:02:08 +1000

    some cleanup

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
-import StartGame from './game/main';
+import StartGame from "./game/main";
 
-document.addEventListener('DOMContentLoaded', () => {
-
-    StartGame('game-container');
-
+document.addEventListener("DOMContentLoaded", () => {
+  StartGame("game-container");
 });
