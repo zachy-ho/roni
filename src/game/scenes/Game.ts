9f424d8e00246fabd2ce2df6e5126d4366c955e3
Maggie Xu <maggiex@canva.com>
2025-07-13 18:09:55 +1000

    add enemy mechanics

--- src/game/scenes/Game.ts
+++ src/game/scenes/Game.ts
@@ -1,11 +1,13 @@
 import { Scene } from "phaser";
 import Colonel from "../objects/Colonel";
+import Enemy from "../objects/Enemy";
 
 export class Game extends Scene {
   camera!: Phaser.Cameras.Scene2D.Camera;
   floor: Phaser.GameObjects.Image;
   msg_text: Phaser.GameObjects.Text;
   colonel: Colonel;
+  enemy: Enemy;
 
   constructor() {
     super("Game");
@@ -13,18 +15,20 @@ export class Game extends Scene {
 
   preload() {
     Colonel.preload(this);
+    Enemy.preload(this);
     this.load.image("floor", "assets/floor.png");
-
   }
 
   create() {
     this.camera = this.cameras.main;
+
 
     this.floor = this.add.image(0, 0, "floor").setOrigin(0, 0);
     this.floor.setDisplaySize(this.camera.width, this.camera.height);
 
     // Create the colonel sprite
     this.colonel = new Colonel(this, 512, 384);
+    this.enemy = Enemy.randomSpawn(this);
 
     this.input.once("pointerdown", () => {
       this.scene.start("GameOver");
@@ -33,5 +37,11 @@ export class Game extends Scene {
 
   update() {
     this.colonel.update();
+
+    if (this.enemy.isDead) {
+      this.enemy = Enemy.randomSpawn(this);
+    }
+
+    this.enemy.update(this.colonel);
   }
 }
